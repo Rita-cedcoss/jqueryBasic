73382fc8106aac576febff8fcfc86fe231b203ca
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-18 12:15:05 +0530

    js jquery file4

--- jquerytask4.js
+++ jquerytask4.js
@@ -1,4 +1,4 @@
-$text="<div id='div'><input type='text'><input type='text'><input type='text'><input type='text'><button class='add'>+</button><button class='divdel'>-</button></div>";
+$text="<div id='div'><input placeholder='enter class' type='text'><input placeholder='enter board' type='text'><input placeholder='enter marks' type='text'><input placeholder='enter division' type='text'><button class='add'>+</button><button class='divdel'>-</button></div>";
     $("#outer").html($text); 
 $(document).ready(function(){
 
