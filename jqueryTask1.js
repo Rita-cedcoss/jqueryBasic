fc4e9370740908d512831db587081790594c4593
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-18 17:12:00 +0530

    jquery js file1

--- jqueryTask1.js
+++ jqueryTask1.js
@@ -1,23 +1,22 @@
 $(document).ready(function(){
     $('#match').click(function()
     {
-      // alert("dcsd");
+     
      var passwd=$('#pswd').val();  
-  //    console.log(passwd);
-  var cfPswd=$('#cfpaswd').val();
-  if(passwd!=cfPswd){
-    // alert("password is not match");
+
+      var cfPswd=$('#cfpaswd').val();
+    if(passwd!=cfPswd){
+    
     $('#display').html('password is not match');
 
-  }
-  else
-  {
+   }
+   else
+   {
     $('#display').html('data is successfuly submited');
     $('#pswd').val('');
     $('#cfpaswd').val('');
-  }
-
-    })
+   }
+   })
 
 
 })
