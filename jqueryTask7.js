c8aa14ed2c078413894461b931f03f72c02c1c41
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-19 12:05:06 +0530

    jquery task7 html file

--- jqueryTask7.js
+++ jqueryTask7.js
@@ -1,8 +1,21 @@
+var flowers = ["rose","lotus","lily","marigold"];
 $(document).ready(function(){
+   
+     $("input").keyup(function(){
 
-    var flowers = ["rose","lotus","lily","marigold"]
-    
-     $( "#input_ID").autocomplete({
-        source: flowers
-     });
+      var matchItem="";
+      for(i=0;i<=flowers.length;i++)
+      { 
+         var value=$("#inptID").val();
+         $match=flowers[i].substring(0,value.length);
+         if($match==value)
+         {
+            matchItem+=" "+flowers[i];
+         }
+         $("#sugestion").html(matchItem);
+      }
+
+
+
+     })
   });
